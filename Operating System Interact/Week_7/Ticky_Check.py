f34b633f275263e026634404bf80403cbb5c0578
Steven <stevenulbrich@me.com>
2022-03-05 21:41:38 -0600

    update to version

--- Operating System Interact/Week_7/Ticky_Check.py
+++ Operating System Interact/Week_7/Ticky_Check.py
@@ -8,60 +8,67 @@ import sys
 
 FIND_ERROR = r'(ERROR ([\w \[]*)) '
 MATCH_USER = r'\((.*?)\)'
-ERROR_IN_LINE = " Error "
+ERROR_IN_LINE = " ERROR "
 INFO_IN_LINE = " INFO "
 NO_TICKET = "Ticket doesn't exist"
 TICKET = "Ticket"
 
 
 error_counter = {}
-error_user = {}
 info_user = {}
 
 
 def search_file(filename: str):
     with open(filename, "r") as my_file:
         for line in my_file:
-            if ERROR_IN_LINE in line:
-                find_error(line)
-                add_user_list(line, 1)
-            elif INFO_IN_LINE in line:
-                add_user_list(line, 2)
+            Date, Server_App, Entry, Message, User = parse_line(line)
+            if Entry == "ERROR":
+                update_error_dic(Message)
+            update_user_dic(Entry, User)
 
 
-def find_error(line: str):
-    match = re.search(FIND_ERROR, line)
-    if match is not None:
-        aux = match.group(1).strip()
-        if aux == TICKET:
-            aux = NO_TICKET
-        if not aux in error_counter:
-            error_counter[aux] = 1
+def update_user_dic(Entry, User):
+    if User not in info_user:
+        if Entry == "ERROR":
+            info_user[User] = [0, 1]
         else:
-            error_counter[aux] += 1
-    return
-
-
-def add_user_list(str, op):
-    match = re.search(MATCH_USER, str)
-    user = match.group(1)
-
-    if op == 1:
-        if not user in error_user:
-            error_user[user] = 1
+            info_user[User] = [1, 0]
+    else:
+        if Entry == "ERROR":
+            info_user[User][1] += 1
         else:
-            error_user[user] += 1
-    elif op == 2:
-        if not user in info_user:
-            info_user[user] = 1
-        else:
-            info_user[user] += 1
-    return
-
-# This function will read the list, arrange it and return a tuple with the dictionary items
+            info_user[User][0] += 1
 
 
-def sort_list(op, list):
+def update_error_dic(error_msg):
+
+    if error_msg not in error_counter:
+        error_counter[error_msg] = 1
+    else:
+        error_counter[error_msg] += 1
+
+
+def parse_line(line: str):
+    # Match attempt failed
+    Date = ""
+    Server_App = ""
+    Entry = ""
+    Message = ""
+    User = ""
+    reobj = re.compile(
+        r"^(?P<Date>\w{3}\s\d{1,2}\s\d{2}:\d{2}:\d{2})(?P<Server_App>.*?:)\s(?P<Entry>INFO|ERROR)\s(?P<Message>.*?)\s\((?P<User>.*?)\)")
+    match = reobj.search(line)
+    if match:
+        Date = match.group("Date")
+        Server_App = match.group("Server_App")
+        Entry = match.group("Entry")
+        Message = match.group("Message")
+        User = match.group("User")
+
+    return Date, Server_App, Entry, Message, User
+
+
+def sort_list(op: int, list: dict):
     if op == 1:
         s = sorted(list.items(), key=operator.itemgetter(1), reverse=True)
     elif op == 2:
@@ -78,53 +85,38 @@ def getErrValue(keyV):
     return 0
 
 
-def write_csv(op: int):
-    if op == 1:
-        with open('user_statistics.csv', 'w', newline='') as output:
-            fieldnames = field_names_header()
-            csvw = csv.DictWriter(output, fieldnames=fieldnames)
-            csvw.writeheader()
-            for key, value in info_user:
-                valError = getErrValue(key)
-                csvw.writerow(
-                    {'Username': key, 'INFO': value, 'ERROR': valError})
-    if op == 2:
-        with open('error_message.csv', 'w', newline='') as output:
-            fieldnames = ['Error', 'Count']
-            csvw = csv.DictWriter(output, fieldnames=fieldnames)
-            csvw.writeheader()
-            for key, value in error_counter:
-                csvw.writerow({'Error': key, 'Count': value})
-    return
+def write_error_messages(error_counter_info):
+    with open('error_message.csv', 'w', newline='') as output:
+        fieldnames = ['Error', 'Count']
+        csvw = csv.DictWriter(output, fieldnames=fieldnames)
+        csvw.writeheader()
+        for key, value in error_counter_info:
+            csvw.writerow({'Error': key, 'Count': value})
+
+
+def write_csv_user_statics(user_info):
+    with open('user_statistics.csv', 'w', newline='') as output:
+        fieldnames = field_names_header()
+        csvw = csv.DictWriter(output, fieldnames=fieldnames)
+        csvw.writeheader()
+        for key, value in user_info:
+            csvw.writerow(
+                {'Username': key, 'INFO': value[0], 'ERROR': value[1]})
 
 
 def field_names_header():
     fieldnames = ['Username', 'INFO', 'ERROR']
     return fieldnames
 
-# This function adds zero to the other dictionary in case that user is not a key, it will add a key with the user and value 0
-
-
-def add_zeros():
-    for user in error_user.keys():
-        if user not in info_user:
-            info_user[user] = 0
-    for user in info_user.keys():
-        if user not in error_user:
-            error_user[user] = 0
-    return
-
 
 def main():
 
-    search_file('syslog.log')
+    search_file('Operating System Interact\Week_7\syslog.log')
 
-    add_zeros()
-    error_counter = sort_list(1, error_counter)
-    error_user = sort_list(2, error_user)
-    info_user = sort_list(2, info_user)
-    write_csv(1)
-    write_csv(2)
+    error_counter_1 = sort_list(1, error_counter)
+    info_user_1 = sort_list(2, info_user)
+    write_csv_user_statics(info_user_1)
+    write_error_messages(error_counter_1)
 
 
 if __name__ == '__main__':
